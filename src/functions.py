d133213f45915ee39b2e7b8aa9743f3dce000171
Ciro Callazans <ccallazans@protonmail.com>
2022-05-05 14:59:00 -0300

    Add comments

--- src/functions.py
+++ src/functions.py
@@ -1,25 +1,30 @@
 import json
 
 def standardize_input(data):
-    # standardize the data format and split using comma
+    # standardize user input to create a list with comma separated values
     selected_data = data.replace(':',',').replace(" ", "")
     splitted_data = selected_data.split(',')
 
     return splitted_data
 
 def read_json(path):
+    # Read json file
     file = open(path)
+
     return json.load(file)
 
 def set_better_choice(hotel_name, price, rating):
+    # Set best hotel option data
     value = {
         'name': hotel_name,
         'price': price,
         'rating': rating
     }
+
     return value
 
-def get_day_price(user_input):
+def get_price_type(user_input):
+    # Check if the day is a weekday or if it is the weekend
     select_index_range = user_input.find('(') + 1
     day = user_input[select_index_range:-1]
 
@@ -36,7 +41,7 @@ def get_day_price(user_input):
 
     return dictionary[day]
 
-def get_reward_program(user_input):
+def get_reward_program_type(user_input):
     return user_input[0]
 
 def get_date_list(user_input):
--- src/my_module.py
+++ src/my_module.py
@@ -1,28 +1,36 @@
 from src.functions import standardize_input, read_json
-from src.functions import get_reward_program, get_date_list, get_day_price
+from src.functions import get_reward_program_type, get_date_list, get_price_type
 from src.functions import set_better_choice
 
-def get_cheapest_hotel(number):   #DO NOT change the function's name
+def get_cheapest_hotel(user_input):   #DO NOT change the function's name
+    #Handle user input
+    handled_user_input = standardize_input(user_input)
 
-    user_input = standardize_input(number)
-    rewards_program = get_reward_program(user_input)
-    date_list = get_date_list(user_input)
+    #Get info about Regular/Rewards user and the sequence of dates 
+    reward_program_type = get_reward_program_type(handled_user_input)
+    date_list = get_date_list(handled_user_input)
 
+    #Read 'hotels.json' data
     hotels_data = read_json('src/hotels.json')
 
     better_choice = None
+    #Iterate through registered hotels on 'hotels.json'
     for hotel in hotels_data['hotels'].keys():
         hotel_price = 0
-
+        #Iterate through user input sequence of dates
         for day in date_list:
-            date_price = get_day_price(day)
-            hotel_price += hotels_data['hotels'][hotel][date_price][rewards_program]
+            price_type = get_price_type(day)
+            hotel_price += hotels_data['hotels'][hotel][price_type][reward_program_type]
 
-        if better_choice == None:
+        #Check if is the first iteration
+        if better_choice == None: 
             better_choice = set_better_choice(hotel, hotel_price, hotels_data['hotels'][hotel]['rating'])
-        elif hotel_price < better_choice['price']:
+        #Check if current hotel price is lower than the 'better_option' price
+        elif hotel_price < better_choice['price']: 
             better_choice = set_better_choice(hotel, hotel_price, hotels_data['hotels'][hotel]['rating'])
-        elif hotel_price == better_choice['price']:
+        #Check if current hotel price is equal to 'better_option' price
+        elif hotel_price == better_choice['price']: 
+            #Check if current hotel rating is greater than 'better_option'
             if hotels_data['hotels'][hotel]['rating'] > better_choice['rating']:
                 better_choice = set_better_choice(hotel, hotel_price, hotels_data['hotels'][hotel]['rating'])
 
