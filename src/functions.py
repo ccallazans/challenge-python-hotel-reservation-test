b507ad845652b5994885d390222556a6faf69681
Ciro Callazans <ccallazans@protonmail.com>
2022-05-05 09:53:19 -0300

    working for initial tests

--- src/functions.py
+++ src/functions.py
@@ -1,19 +1,42 @@
-def standardize_data(data):
+import json
+
+def standardize_input(data):
     # standardize the data format and split using comma
     selected_data = data.replace(':',',').replace(" ", "")
     splitted_data = selected_data.split(',')
 
     return splitted_data
 
+def get_day_price(user_input):
+    select_index_range = user_input.find('(') + 1
+    day = user_input[select_index_range:-1]
+
+    dictionary = {
+        "mon": 'weekday_price',
+        "tues": 'weekday_price',
+        "wed": 'weekday_price',
+        "thur": 'weekday_price',
+        "fri": 'weekday_price',
+        "sat": 'weekend_price',
+        "sun": 'weekend_price',
+    }
+
+    return dictionary[day]
+
+def set_better_choice(hotel_name, price, rating):
+    value = {
+        'name': hotel_name,
+        'price': price,
+        'rating': rating
+    }
+    return value
+
+def read_json(path):
+    file = open(path)
+    return json.load(file)
+
 def get_reward_program(user_input):
     return user_input[0]
 
-def get_date_sequence(user_input):
+def get_date_list(user_input):
     return user_input[1:]
-
-def is_weekend(user_input):
-    
-
-
-
-
--- src/my_module.py
+++ src/my_module.py
@@ -1,15 +1,30 @@
-from functions import standardize_data, get_reward_program, get_date_sequence
+from src.functions import standardize_input, read_json
+from src.functions import get_reward_program, get_date_list, get_day_price
+from src.functions import set_better_choice
 
 def get_cheapest_hotel(number):   #DO NOT change the function's name
 
-    data = standardize_data(number)
-    select_rewards_program = get_reward_program(data)
-    select_date_sequence = get_date_sequence(data)
+    user_input = standardize_input(number)
+    rewards_program = get_reward_program(user_input)
+    date_list = get_date_list(user_input)
 
-    
+    hotels_data = read_json('src/hotels.json')
 
+    better_choice = None
+    for hotel in hotels_data['hotels'].keys():
+        hotel_price = 0
 
-    cheapest_hotel = "cheapest_hotel_name"
+        for day in date_list:
+            date_price = get_day_price(day)
+            hotel_price += hotels_data['hotels'][hotel][date_price][rewards_program]
+
+        if better_choice == None:
+            better_choice = set_better_choice(hotel, hotel_price, hotels_data['hotels'][hotel]['rating'])
+        elif hotel_price < better_choice['price']:
+            better_choice = set_better_choice(hotel, hotel_price, hotels_data['hotels'][hotel]['rating'])
+        elif hotel_price == better_choice['price']:
+            if hotels_data['hotels'][hotel]['rating'] > better_choice['rating']:
+                better_choice = set_better_choice(hotel, hotel_price, hotels_data['hotels'][hotel]['rating'])
+
+    cheapest_hotel = better_choice['name']
     return cheapest_hotel
-
-get_cheapest_hotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)")
