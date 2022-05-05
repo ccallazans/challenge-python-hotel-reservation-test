b68bde63921e8c16aff1cea61b0714a2bfb80180
Ciro Callazans <ccallazans@protonmail.com>
2022-05-05 20:28:53 -0300

    removed for loop, reducing complexity. Also added comments on functions.py

--- src/functions.py
+++ src/functions.py
@@ -1,19 +1,35 @@
 import json
 
 def standardize_input(data):
-    # standardize user input to create a list with comma separated values
+    """
+    Standardize user input to create a list
+    Parameter data: user input string
+    Returns the list [<reward_program_type>,<day1>,<day2>,<day3>]
+    """
     selected_data = data.replace(':',',').replace(" ", "")
     splitted_data = selected_data.split(',')
 
     return splitted_data
 
 def read_json(path):
+    """
+    Read json file
+    Parameter path: the json file path
+    Returns a python dictionary with the file data
+    """
     # Read json file
     file = open(path)
 
     return json.load(file)
 
 def set_better_choice(hotel_name, price, rating):
+    """
+    Create dictionary containing informations about the hotel
+    Parameter hotel_name: name of the hotel
+    Parameter price: price of the hotel
+    Parameter rating: rating of the hotel
+    Returns a python dictionary with hotel info
+    """
     # Set best hotel option data
     value = {
         'name': hotel_name,
@@ -24,6 +40,11 @@ def set_better_choice(hotel_name, price, rating):
     return value
 
 def get_price_type(user_input):
+    """
+    Shows if a day is a weekday or weekend
+    Parameter user_input: reservation date string
+    Returns string -> <day price>
+    """
     # Check if the day is a weekday or if it is the weekend
     select_index_range = user_input.find('(') + 1
     day = user_input[select_index_range:-1]
@@ -42,7 +63,27 @@ def get_price_type(user_input):
     return dictionary[day]
 
 def get_reward_program_type(user_input):
+    """
+    Get if user is a regular or rewards user
+    Parameter user_input: List -> [<reward_program_type>,<day1>,<day2>,<day3>]
+    """
     return user_input[0]
 
-def get_date_list(user_input):
-    return user_input[1:]
+def get_date_type_count(user_input):
+    """
+    Count how many weekdays and weekend days are in the user input
+    Parameter user_input: List -> [<reward_program_type>,<day1>,<day2>,<day3>]
+    Returns dict
+    """
+    date_list = user_input[1:]
+
+    price_type = []
+    for day in date_list:
+        price_type.append(get_price_type(day))
+
+    days_count = {
+        'weekday_price': price_type.count('weekday_price'),
+        'weekend_price': price_type.count('weekend_price'),
+    }
+
+    return days_count
--- src/my_module.py
+++ src/my_module.py
@@ -1,38 +1,34 @@
 from src.functions import standardize_input, read_json
-from src.functions import get_reward_program_type, get_date_list, get_price_type
+from src.functions import get_reward_program_type, get_date_type_count
 from src.functions import set_better_choice
 
 def get_cheapest_hotel(user_input):   #DO NOT change the function's name
-    #Handle user input
+
     handled_user_input = standardize_input(user_input)
-
-    #Get info about Regular/Rewards user and the sequence of dates 
     reward_program_type = get_reward_program_type(handled_user_input)
-    date_list = get_date_list(handled_user_input)
+    date_type_count = get_date_type_count(handled_user_input)
 
-    #Read 'hotels.json' data
     hotels_data = read_json('src/hotels.json')
+    hotels_data = hotels_data['hotels']
 
     better_choice = None
     #Iterate through registered hotels on 'hotels.json'
-    for hotel in hotels_data['hotels'].keys():
-        hotel_price = 0
-        #Iterate through user input sequence of dates
-        for day in date_list:
-            price_type = get_price_type(day)
-            hotel_price += hotels_data['hotels'][hotel][price_type][reward_program_type]
+    for hotel in hotels_data.keys():
+        weekday_price = hotels_data[hotel]['weekday_price'][reward_program_type] * date_type_count['weekday_price']
+        weekend_price = hotels_data[hotel]['weekend_price'][reward_program_type] * date_type_count['weekend_price']
+        hotel_price = weekday_price + weekend_price
 
         #Check if is the first iteration
         if better_choice == None: 
-            better_choice = set_better_choice(hotel, hotel_price, hotels_data['hotels'][hotel]['rating'])
+            better_choice = set_better_choice(hotel, hotel_price, hotels_data[hotel]['rating'])
         #Check if current hotel price is lower than the 'better_option' price
         elif hotel_price < better_choice['price']: 
-            better_choice = set_better_choice(hotel, hotel_price, hotels_data['hotels'][hotel]['rating'])
+            better_choice = set_better_choice(hotel, hotel_price, hotels_data[hotel]['rating'])
         #Check if current hotel price is equal to 'better_option' price
         elif hotel_price == better_choice['price']: 
             #Check if current hotel rating is greater than 'better_option'
-            if hotels_data['hotels'][hotel]['rating'] > better_choice['rating']:
-                better_choice = set_better_choice(hotel, hotel_price, hotels_data['hotels'][hotel]['rating'])
+            if hotels_data[hotel]['rating'] > better_choice['rating']:
+                better_choice = set_better_choice(hotel, hotel_price, hotels_data[hotel]['rating'])
 
     cheapest_hotel = better_choice['name']
     return cheapest_hotel
